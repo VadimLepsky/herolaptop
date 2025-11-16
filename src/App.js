11242d6f6765b8f8d8d3116ddcd4a3fa031fcb26
VadimLepsky <vadimlepskiy.d@gmail.com>
2025-11-16 19:35:59 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -48,11 +48,15 @@ export default function App() {
     <Canvas camera={{ position: [-5, 0, -15], fov: 55 }} style={{ background: '#FFFFFF' }}>
       <pointLight position={[10, 10, 10]} intensity={1.5} />
       <Suspense fallback={null}>
-        <group rotation={[0, Math.PI, 0]} position={[0, 1, 0]}> scale={0.8}>
-          <Model />
-        </group>
-        <Environment preset="city" />
-      </Suspense>
+  <group 
+    rotation={[0, Math.PI, 0]} 
+    position={[0, 1, 0]} 
+    scale={0.8}
+  >
+    <Model />
+  </group>
+  <Environment preset="city" />
+</Suspense>
       {/* <ContactShadows position={[0, -4.5, 0]} scale={20} blur={2} far={4.5} /> */}
       <OrbitControls enablePan={false} enableZoom={false} minPolarAngle={Math.PI / 2.2} maxPolarAngle={Math.PI / 2.2} />
     </Canvas>
