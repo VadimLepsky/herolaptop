8074f2be65d0cb753b90df597509bc37ef8b82a4
VadimLepsky <vadimlepskiy.d@gmail.com>
2026-01-18 19:13:05 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ export default function App() {
   <Environment preset="city" intensity={0.01} />
 </Suspense>
       {/* <ContactShadows position={[0, -4.5, 0]} scale={20} blur={2} far={4.5} /> */}
-     // <OrbitControls enablePan={false} enableZoom={false} minPolarAngle={Math.PI / 2.2} maxPolarAngle={Math.PI / 2.2} />
+     {/* <OrbitControls enablePan={false} enableZoom={false} minPolarAngle={Math.PI / 2.2} maxPolarAngle={Math.PI / 2.2} /> */}
     </Canvas>
   )
 }
