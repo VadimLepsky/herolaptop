0ebf0261049ff5f64f388ba18218f8a86128df70
VadimLepsky <vadimlepskiy.d@gmail.com>
2026-01-19 12:59:36 +0100

    Update HeroPage.js

--- src/HeroPage.js
+++ src/HeroPage.js
@@ -1,13 +1,14 @@
-// src/HeroPage.js
 export default function HeroPage() {
   return (
     <div className="w-full h-full overflow-hidden rounded-[14px] bg-black">
-      <iframe
-        src="https://iframe.mediadelivery.net/embed/574718/2e1ef19b-af2e-4c5b-b1d4-62d6f67882ec?autoplay=true&loop=true&muted=true&preload=true&responsive=false"
-        title="Stream video"
-        allow="autoplay; encrypted-media; picture-in-picture"
-        allowFullScreen
-        className="w-full h-full border-0"
+      <video
+        src="https://iframe.mediadelivery.net/play/574718/2e1ef19b-af2e-4c5b-b1d4-62d6f67882ec"
+        autoPlay
+        loop
+        muted
+        playsInline
+        preload="auto"
+        className="w-full h-full object-cover"
       />
     </div>
   )
